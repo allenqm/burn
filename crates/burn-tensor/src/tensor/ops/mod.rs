ff4b976f21547bafe9fbf285634ab61ab2942ca5
allenqm <adam.maruf@gmail.com>
2025-06-04 13:24:52 -0400

    (TODO) Add an ExternalMemoryDescriptor and Error type to burn-tensor...
    Note: This currently has issues in that it makes burn-tensor aware
    of external memory sources (in this case cudarc for cuda programs).
    
    We don't want burn-tensor to have to include all possible external sources.
    
    The ExternalMemoryDescriptor allows us to have a streamlined API
    for from_ext_data(desc, device) for all sources. desc is a single
    type.

--- crates/burn-tensor/src/tensor/ops/mod.rs
+++ crates/burn-tensor/src/tensor/ops/mod.rs
@@ -17,3 +17,30 @@ pub use modules::*;
 pub use qtensor::*;
 pub use tensor::*;
 pub use transaction::*;
+
+use super::DType;
+
+/// Error type
+#[derive(Debug)]
+pub enum ExternalMemoryError {
+    UnsupportedMemoryType,
+    AllocationFailed,
+    InvalidDescriptor,
+}
+
+/// A description of GPU memory managed by a process external
+/// to burn, for a variety of sources
+// TODO(aqm): There shouldn't be backend specific code in this crate...
+// Where would be a more fitting place to put this?
+#[derive(Debug)]
+pub enum ExternalMemoryDescriptor {
+    /// For Cuda
+    CudaPtr {
+        ptr: cudarc::driver::sys::CUdeviceptr,
+        size_bytes: usize,
+        shape: Vec<usize>,
+        elem_size: usize,
+        dtype: DType,
+    },
+    // Future: Metal, Vulkan, etc.
+}
